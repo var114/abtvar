905dea40513dd36e385637ab0aea8cbf0d320544
Patil Varvarian <Patil.Varvarian@gmail.com>
2013-12-24 13:17:53 -0800

    version 0.1.0

--- lib/abtvar/answers.rb
+++ lib/abtvar/answers.rb
@@ -1,12 +1,5 @@
 class Bio
 
-attr_reader :answer_x, :answer_y
-
-  def initialize 
-    answer_x = ''
-    answer_y= ''
-  end
-
   def about(answer)
     if answer == 'Y'
        "Because she can code in Ruby"
